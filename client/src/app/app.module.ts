984693e9cc465a1bffe7c68673a13ac3bdac7682
Jerin George <jerin.george@medtronic.com>
2021-01-17 12:04:38 -0700

    end of section 12

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,19 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import {HttpClientModule, HTTP_INTERCEPTORS} from '@angular/common/http'
-
+import {HttpClientModule, HTTP_INTERCEPTORS} from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { CoreModule } from './core/core.module';
-import { ShopModule } from './shop/shop.module';
 import { HomeModule } from './home/home.module';
 import { ErrorInterceptor } from './core/Interceptors/error.interceptor';
+import { NgxSpinnerModule } from 'ngx-spinner';
+import { LoadingInterceptor } from './core/Interceptors/loading.interceptor';
 
 
 @NgModule({
   declarations: [
-    AppComponent,    
+    AppComponent
   ],
   imports: [
     BrowserModule,
@@ -21,9 +21,11 @@ import { ErrorInterceptor } from './core/Interceptors/error.interceptor';
     BrowserAnimationsModule,
     HttpClientModule,
     CoreModule,
-    HomeModule
+    HomeModule,
+    NgxSpinnerModule
   ],
-  providers: [{provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true}],
+  providers: [{provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true},
+              {provide: HTTP_INTERCEPTORS, useClass: LoadingInterceptor, multi: true}],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- client/src/app/core/Interceptors/error.interceptor.ts
+++ client/src/app/core/Interceptors/error.interceptor.ts
@@ -7,7 +7,7 @@ import {
 } from '@angular/common/http';
 import { Observable, throwError } from 'rxjs';
 import { NavigationExtras, Router } from '@angular/router';
-import { catchError } from 'rxjs/operators';
+import { catchError, delay } from 'rxjs/operators';
 import { ToastrService } from 'ngx-toastr';
 
 @Injectable()
@@ -17,11 +17,10 @@ export class ErrorInterceptor implements HttpInterceptor {
 
   intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
     return next.handle(request).pipe(
-
       catchError(error => {
-        if(error)
+        if (error)
         {
-          if(error.status ===400)
+          if (error.status === 400)
           {
             if (error.error.errors) {
               throw error.error;
@@ -29,21 +28,19 @@ export class ErrorInterceptor implements HttpInterceptor {
               this.toastr.error(error.error.message, error.error.statusCode);
             }
           }
-          if(error.status ===401)
+          if (error.status === 401)
           {
             this.toastr.error(error.error.message, error.error.statusCode);
-            
           }
-          if(error.status === 404)
+          if (error.status === 404)
           {
             this.router.navigateByUrl('/not-found');
 
           }
-          if(error.status === 500)
+          if (error.status === 500)
           {
             const navigationExtras: NavigationExtras = {state: {error: error.error}};
             this.router.navigateByUrl('/server-error', navigationExtras);
-            
           }
         }
         return throwError(error);
